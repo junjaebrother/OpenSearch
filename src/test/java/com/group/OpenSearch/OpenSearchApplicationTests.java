8d8e7d91b7db0fe8fe93e10a0bce7ef964667682
한재준 <gkswowns25@naver.com>
2024-09-26 23:04:05 +0900

    init commit

---
+++ src/test/java/com/group/OpenSearch/OpenSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.OpenSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
